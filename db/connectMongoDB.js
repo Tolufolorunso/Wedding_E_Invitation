1eb01135d2403eba3c5f2902f76977139b8d82d1
Tolulope Folorunso <tolufolorunso@yahoo.com>
2025-01-09 18:40:19 +0100

    updated

--- db/connectMongoDB.js
+++ db/connectMongoDB.js
@@ -1,8 +1,8 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectMongoDB = async () => {
   let mongodb_uri;
-  if (process.env.NEXT_ENV === "development") {
+  if (process.env.NEXT_ENV === 'development') {
     mongodb_uri = process.env.MONGODB_URI_LOCAL;
   } else {
     mongodb_uri = process.env.MONGODB_URI;
@@ -10,7 +10,7 @@ const connectMongoDB = async () => {
   try {
     await mongoose.connect(mongodb_uri);
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error('MongoDB connection error:', error);
   }
 };
 
